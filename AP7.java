6fb79d80807d42b61cb3b8153b939223ba7bfbaa
Abhijeet Rana <abhijeet4rana@gmail.com>
2022-11-22 22:09:21 +0530

    Second Commit

---
+++ AP7.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+class AP7{
+	public static void main(String[] args){
+		System.out.print("Original Sentence: ");
+		String s = "This is a reverse Sentence in Java";
+		System.out.println(s);
+		String rev = reverseSentence(s);
+		System.out.print("Reverse Sentence: ");
+		System.out.println(rev);
+	}
+	public static String reverseSentence(String str){  
+    String[] words=str.split("\\s");  
+    String reverseWord="";  
+    for(String w:words){  
+        StringBuilder sb=new StringBuilder(w);  
+        sb.reverse();  
+        reverseWord+=sb.toString()+" ";  
+    }  
+    return reverseWord.trim();  
+}  
+}
